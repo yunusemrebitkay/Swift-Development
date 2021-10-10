d9c7db17f7827084e5ae51019dbaba93f1cbd1da
Yunus Emre Bitkay <68568743+yunusemrebitkay@users.noreply.github.com>
2021-10-11 01:43:58 +0300

    Add files via upload

---
+++ MusicianClass/MusicianClass/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  MusicianClass
+//
+//  Created by Yunus Emre Bitkay on 11.10.2021.
+//
+
+import Foundation
+
+var james = Musicians(nameInit: "James", ageInit: 25, instrumentInit: "Guitar", typeInit: .Drummer)
+james.sing()
+
+var Müslüm = ExtraMusician(nameInit: "Müslüm", ageInit: 40, instrumentInit: "Saz", typeInit: .Violenist)
+Müslüm.sing()
